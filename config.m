6b727acc086750c0f4982d22d8521722f5f524cf
Leo Paillier <leo.paillier@gmail.com>
2017-01-30 11:10:23 +0100

    small update

---
+++ config.m
@@ -0,0 +1,7 @@
+% Configuration script: should be run before any other script to ensure
+% proper configuration.
+% Store all parameters in the cfg variable.
+
+cfg.dir_test  = 'Data/test/pictures_test/';
+cfg.dir_train = 'Data/train/pictures_train/';
+cfg.vgg_path  = 'vgg-face.mat';
