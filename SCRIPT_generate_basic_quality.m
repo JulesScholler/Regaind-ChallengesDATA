12bf2c834ab10fe82b75011557fb7169e9a8ce4e
Leo Paillier <leo.paillier@gmail.com>
2017-02-07 15:58:02 +0100

    basic quality

---
+++ SCRIPT_generate_basic_quality.m
@@ -0,0 +1,83 @@
+% Analyze all pictures and compute the basic quality features
+%
+% Return values:
+%   - basic_qual_test: basic quality features for the test dataset
+%   - basic_qual_train: basic quality features for the train dataset
+
+% Load the config parameters
+SCRIPT_config;
+
+% Check if the features already exist
+if exist('basic_qual_features.mat','file')
+    % Load the data form the pre-existing .mat file
+    disp('Loading data from basic_qual_features.mat')
+    load('basic_qual_features.mat');
+else
+    % Initialize variables   
+    basic_qual_test  = zeros(3000,2);   % test features
+    basic_qual_train = zeros(10000,2);  % train features
+
+    % Loop through all training images
+    disp('Train basic quality starting...')
+    for i=1:10000
+        % Open image
+        im_fn   = [num2str(i) '.jpg'];              % image filename
+        im      = imread([cfg.dir_train im_fn]);    % open image
+        if ndims(im) == 2                           % convert to rgb
+            im = cat(3, im, im, im);
+        end
+        
+        % Compute the contrast quality
+        im_gray = rgb2gray(im);                         % grayscale
+        im_eq   = double(histeq(im_gray));              % hist equalization
+        im_diff = (double(im_gray) - im_eq).^2;         % diff square
+        basic_qual_train(i,1) = sqrt(mean(im_diff(:))); % mean
+        
+        % Compute the exposure quality
+        im_y    = rgb2ycbcr(im);                        % change space
+        im_y    = im_y(:,:,1);                          % luminance
+        skew_y  = skewness(histcounts(im_y,linspace(0,255,256)));
+        basic_qual_train(i,2) = -abs(skew_y);           % exposure
+        
+        % Display progress
+        if mod(100*i/10000,5)==0
+            fprintf('Train basic quality: %i%%\n',uint8(100*i/10000));
+        end
+    end
+    disp('Train basic quality done.')
+
+    % Loop through all test images
+    disp('Test basic quality starting...')
+    for i=1:3000
+        % Open image
+        im_fn    = [num2str(i+10000) '.jpg'];       % image filename        
+        im      = imread([cfg.dir_test im_fn]);     % open image
+        if ndims(im) == 2                           % convert to rgb
+            im = cat(3, im, im, im);
+        end
+        
+        % Compute the contrast quality
+        im_gray = rgb2gray(im);                         % grayscale
+        im_eq   = double(histeq(im_gray));              % hist equalization
+        im_diff = (double(im_gray) - im_eq).^2;         % diff square
+        basic_qual_test(i,1) = sqrt(mean(im_diff(:)));  % mean
+        
+        % Compute the exposure quality
+        im_y    = rgb2ycbcr(im);                        % change space
+        im_y    = im_y(:,:,1);                          % luminance
+        skew_y  = skewness(histcounts(im_y,linspace(0,255,256)));
+        basic_qual_test(i,2) = -abs(skew_y);            % exposure
+
+        % Display progress
+        if mod(100*i/3000,5)==0
+            fprintf('Test basic quality: %i%%\n',uint8(100*i/3000));
+        end
+    end
+    disp('Test basic quality done.')
+
+    % Save data
+    save('basic_qual_features.mat','basic_qual_train','basic_qual_test')
+    
+    % Clear variables
+    clear ans hist i im im_diff im_eq im_fn im_gray im_y skew_y;
+end
---
+++ SCRIPT_model_score.m
@@ -0,0 +1,53 @@
+% Train a SVM using the pre-generated features and save it in the file
+% SVM_model.mat
+%
+% Return values:
+%   - SVM_model: trained SVM model
+
+% Load the config parameters
+SCRIPT_config;
+
+% Load the data
+SCRIPT_load_meta;               % metadata for test and train
+SCRIPT_load_impact;             % impact for train
+SCRIPT_load_score;              % aesthetics score for train
+SCRIPT_generate_histo;          % histogram for test and train
+SCRIPT_generate_vgg;            % vgg face for test and train
+SCRIPT_generate_basic_quality;  % overall image basic quality
+
+% Assemble the data
+% data_train = meta_train;
+data_train = [meta_train basic_qual_train yahoo_train];
+
+
+% Set parameters
+n_fold = 10;            % cross-validation parameters
+
+% Do cross-validation training for evaluation
+idx = randperm(10000);          % randomisation indices
+rank_eval = zeros(1,n_fold);    % pearson ranking correlation
+for i = 1:n_fold
+    % Generate the CV train and test dataset and corresponding scores
+    sep_inf = 1+ floor((i-1)*10000/n_fold);         % inferior separator
+    sep_sup = floor(i*10000/n_fold);                % superior separator  
+    idx_CV_test  = idx(sep_inf:sep_sup);            % CV test indices
+    idx_CV_train = idx;                             % CV train indices
+    idx_CV_train(sep_inf:sep_sup) = [];             % remove test indices
+    data_CV_test   = data_train(idx_CV_test,:);     % CV test data   
+    data_CV_train  = data_train(idx_CV_train,:);    % CV train data
+    score_CV_test  = score_train(idx_CV_test);      % CV test score
+    score_CV_train = score_train(idx_CV_train);     % CV train score
+    
+    % Train the SVM
+    SVM_model = fitrsvm(data_CV_train,score_CV_train,'KernelFunction','Linear','Standardize',true);
+%     SVM_model = fitrlinear(data_CV_train,score_CV_train);
+    
+    % Predict scores
+    predict_CV_test = predict(SVM_model,data_CV_test);
+    
+    % Evaluate method
+    [rank_eval(i),~] = corr(score_CV_test,predict_CV_test,'type','Spearman')    
+end
+
+% Display results
+fprintf('SVM cross validation done, average: %f\n',mean(rank_eval))
