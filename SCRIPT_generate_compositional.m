dffbbe3907c4adcf6a3686df5b35bc2f327c817a
Leo Paillier <leo.paillier@gmail.com>
2017-02-08 19:38:14 +0100

    cleanup

--- SCRIPT_generate_compositional.m
+++ SCRIPT_generate_compositional.m
@@ -8,19 +8,6 @@
 SCRIPT_config;
 
 % Check if the features already exist
-<<<<<<< HEAD
-if exist('spectral_saliency_features.mat','file')
-    % Load the data form the pre-existing .mat file
-    disp('Loading data from spectral_saliency_features.mat')
-    load('spectral_saliency_features.mat');
-else
-    % Initialize variables   
-    spectral_saliency_train= zeros(10000,9);   % test features
-    spectral_saliency_test = zeros(3000,9);  % train features
-
-    % Loop through all training images
-    disp('Train basic quality starting...')
-=======
 if exist('compositional_features.mat','file')
     % Load the data form the pre-existing .mat file
     disp('Loading data from compositional_features.mat')
@@ -31,7 +18,6 @@ else
     disp('Train composition starting...')
     hist_train=zeros(10000,32);
     contrast_train=zeros(10000,2);
->>>>>>> origin/master
     for i=1:10000
         % Open image
         im_fn   = [num2str(i) '.jpg'];              % image filename
@@ -40,19 +26,14 @@ else
             im = cat(3, im, im, im);
         end
         
-        spectral_saliency_train(i,:)=eval_spectral_saliency(im);
+        hist_train(i,:)=eval_color(im);
+        contrast_train(i,:)=eval_contrast(im);
         
         % Display progress
         if mod(100*i/10000,5)==0
             fprintf('Train composition: %i%%\n',uint8(100*i/10000));
         end
     end
-<<<<<<< HEAD
-    disp('Train basic quality done.')
-
-    % Loop through all test images
-    disp('Test basic quality starting...')
-=======
     compositional_train=[hist_train contrast_train];
     disp('Train composition done.')
 
@@ -60,7 +41,6 @@ else
     disp('Test composition starting...')
     hist_test=zeros(3000,32);
     contrast_test=zeros(3000,2);
->>>>>>> origin/master
     for i=1:3000
         % Open image
         im_fn    = [num2str(i+10000) '.jpg'];       % image filename        
@@ -69,7 +49,8 @@ else
             im = cat(3, im, im, im);
         end
         
-        spectral_saliency_test(i,:)=eval_spectral_saliency(im);
+        hist_test(i,:)=eval_color(im);
+        contrast_test(i,:)=eval_contrast(im);
 
         % Display progress
         if mod(100*i/3000,5)==0
@@ -77,13 +58,9 @@ else
         end
     end
     
-<<<<<<< HEAD
-    disp('Test basic quality done.')
-=======
     compositional_test=[hist_test contrast_test];
     disp('Test composition done.')
->>>>>>> origin/master
 
     % Save data
-    save('spectral_saliency_features.mat','spectral_saliency_train','spectral_saliency_test')
+    save('compositional_features.mat','compositional_train','compositional_test')
 end
